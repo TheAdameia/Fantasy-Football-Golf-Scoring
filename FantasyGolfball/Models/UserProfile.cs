649ad850f3c1a8263f7a84b55831b86e153fa66c
Adameia Ramsey <adameiaproductions@gmail.com>
2024-09-02 00:41:21 -0500

    saving

--- FantasyGolfball/Models/UserProfile.cs
+++ FantasyGolfball/Models/UserProfile.cs
@@ -5,10 +5,6 @@ namespace FantasyGolfball.Models;
 public class UserProfile
 {
     public int Id { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string Address { get; set; }
-
     public string IdentityUserId { get; set; }
 
     public IdentityUser IdentityUser { get; set; }
