ce68d3fbd43a05abeff7bb5269728eeb6037d816
Adameia Ramsey <adameiaproductions@gmail.com>
2024-11-22 16:09:41 -0600

    saving

---
+++ FantasyGolfball/client/src/components/draft/DraftPage.jsx
@@ -0,0 +1,11 @@
+import { useAppContext } from "../../contexts/AppContext"
+
+
+export const DraftPage = () => {
+    const { players } = useAppContext()
+
+    // I will need a mockup of this UI before I do anything to actually design it.
+    return (
+        <div></div>
+    )
+}
