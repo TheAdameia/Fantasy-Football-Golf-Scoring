2f60686bf9bc334106feae12ee5b206145df6c86
Adameia Ramsey <adameiaproductions@gmail.com>
2025-02-01 14:05:59 -0600

    functional team display added

--- FantasyGolfball/client/src/components/draft/DraftTeamDisplay.jsx
+++ FantasyGolfball/client/src/components/draft/DraftTeamDisplay.jsx
@@ -1,13 +1,36 @@
-import { useContext } from "react"
-import { DraftContext } from "./DraftPage"
+import { useEffect, useState } from "react"
+import { useAppContext } from "../../contexts/AppContext"
 
 
 export const DraftTeamDisplay = () => {
-    const { draftState } = useContext(DraftContext)
+    const { roster } = useAppContext()
+    const [rplayers, SetRPlayers] = useState()
 
-    // if the roster for this user has at least one player in it, display those players; otherwise display no players
+    useEffect(() => {
+        if (!roster) {
+            return
+        }
+        if (roster.rosterPlayers) {
+            SetRPlayers(roster.rosterPlayers)
+        }
+    }, [roster])
+
+    if (!rplayers) {
+        return (
+            <div>No players drafted yet</div>
+        )
+    }
 
     return (
-        <div>No players drafted yet</div>
+        <div>
+            {rplayers.map((rp) => {
+                return (
+                    <div key={rp.player.playerId}>
+                        <div>{rp.player.position.positionShort}</div>
+                        <div>{rp.player.playerFirstName} {rp.player.playerLastName}</div>
+                    </div>
+                )
+            })}
+        </div>
     )
 }
