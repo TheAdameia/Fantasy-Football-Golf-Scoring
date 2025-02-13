f1703b97259e1008fcb7c18a508d4ea54a2e816f
Adameia Ramsey <adameiaproductions@gmail.com>
2025-02-12 19:03:34 -0600

    expanded matchups class to be more flexible

---
+++ FantasyGolfball/Models/MatchupUser.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace FantasyGolfball.Models;
+
+public class MatchupUser
+{
+    public int MatchupUserId { get; set; }
+    public int UserProfileId { get; set; }
+    public UserProfile userProfile { get; set; }
+    public int MatchupId { get; set; }
+    public Matchup matchup { get; set; }
+}
