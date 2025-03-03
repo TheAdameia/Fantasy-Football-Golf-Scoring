bb43ddced61960b39b2d8c99a89ad7e53ad362a2
Adameia Ramsey <adameiaproductions@gmail.com>
2025-03-03 16:14:10 -0600

    saving

---
+++ FantasyGolfball/Models/Season.cs
@@ -0,0 +1,19 @@
+
+
+namespace FantasyGolfball.Models;
+
+public class Season
+{
+    public int SeasonId { get; set; }
+    public int SeasonYear { get; set; }
+    // how do I want to indicate that each week is done? just a bool? That's probably best.
+    public bool Week1Moves { get; set; }
+    // signalling the scoring calculation would probably be another chronjob thing?
+
+
+    // the idea is that... there will be premade seasons for the actual, real season.
+    // but if people want to "replay" a past season, a new instance of the class is created
+    // with an identical year, and it can proceed as fast as they want it to.
+
+    public bool RealSeason { get; set; }
+}
