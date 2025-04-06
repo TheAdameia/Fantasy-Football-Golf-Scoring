93ed777a72ff86ddf59998f819188e8b25d2db1d
Adameia Ramsey <adameiaproductions@gmail.com>
2025-04-06 15:48:09 -0500

    empty roster slots now shown

--- FantasyGolfball/client/src/components/roster/RosterCard.jsx
+++ FantasyGolfball/client/src/components/roster/RosterCard.jsx
@@ -2,6 +2,8 @@ import { Table } from "reactstrap"
 import { RosterPlayerCard } from "./RosterPlayerCard"
 import { useAppContext } from "../../contexts/AppContext";
 import "./Roster.css"
+import { BlankPlayerCard } from "../matchups/BlankPlayerCard";
+import { BlankRosterPlayerCard } from "./BlankRosterPlayerCard";
 
 
 export const RosterCard = () => {
@@ -17,10 +19,10 @@ export const RosterCard = () => {
                 <thead>
                     <tr>
                         <th>
-                            pos ddown
+                            Select
                         </th>
                         <th>
-                            Activity
+                            Position
                         </th>
                         <th>
                             Status
@@ -46,101 +48,128 @@ export const RosterCard = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    {roster.rosterPlayers
-                        .filter((rp) => rp.rosterPosition === "QB1")
-                        .map((rp) => {
-                            return (
-                                <RosterPlayerCard
-                                    rp={rp}
-                                    key={`rp-${rp.rosterPlayerId}`}
-                                ></RosterPlayerCard>
-                            )
-                    })}
-                    {roster.rosterPlayers
-                        .filter((rp) => rp.rosterPosition === "WR1")
-                        .map((rp) => {
-                            return (
-                                <RosterPlayerCard
-                                    rp={rp}
-                                    key={`rp-${rp.rosterPlayerId}`}
-                                ></RosterPlayerCard>
-                            )
-                    })}
-                    {roster.rosterPlayers
-                        .filter((rp) => rp.rosterPosition === "WR2")
-                        .map((rp) => {
-                            return (
-                                <RosterPlayerCard
-                                    rp={rp}
-                                    key={`rp-${rp.rosterPlayerId}`}
-                                ></RosterPlayerCard>
-                            )
-                    })}
-                    {roster.rosterPlayers
-                        .filter((rp) => rp.rosterPosition === "RB1")
-                        .map((rp) => {
-                            return (
-                                <RosterPlayerCard
-                                    rp={rp}
-                                    key={`rp-${rp.rosterPlayerId}`}
-                                ></RosterPlayerCard>
-                            )
-                    })}
-                    {roster.rosterPlayers
-                        .filter((rp) => rp.rosterPosition === "RB2")
-                        .map((rp) => {
-                            return (
-                                <RosterPlayerCard
-                                    rp={rp}
-                                    key={`rp-${rp.rosterPlayerId}`}
-                                ></RosterPlayerCard>
-                            )
-                    })}
-                    {roster.rosterPlayers
-                        .filter((rp) => rp.rosterPosition === "TE1")
-                        .map((rp) => {
-                            return (
-                                <RosterPlayerCard
-                                    rp={rp}
-                                    key={`rp-${rp.rosterPlayerId}`}
-                                ></RosterPlayerCard>
-                            )
-                    })}
-                    {roster.rosterPlayers
-                        .filter((rp) => rp.rosterPosition === "FLEX")
-                        .map((rp) => {
-                            return (
-                                <RosterPlayerCard
-                                    rp={rp}
-                                    key={`rp-${rp.rosterPlayerId}`}
-                                ></RosterPlayerCard>
-                            )
-                    })}
-                    {roster.rosterPlayers
-                        .filter((rp) => rp.rosterPosition === "K")
-                        .map((rp) => {
-                            return (
-                                <RosterPlayerCard
-                                    rp={rp}
-                                    key={`rp-${rp.rosterPlayerId}`}
-                                ></RosterPlayerCard>
-                            )
-                    })}
-                    {roster.rosterPlayers
-                        .filter((rp) => rp.rosterPosition === "DEF")
-                        .map((rp) => {
-                            return (
-                                <RosterPlayerCard
-                                    rp={rp}
-                                    key={`rp-${rp.rosterPlayerId}`}
-                                ></RosterPlayerCard>
-                            )
-                    })}
+                    {roster.rosterPlayers.some((rp) => rp.rosterPosition === "QB1") ?
+                        roster.rosterPlayers
+                            .filter((rp) => rp.rosterPosition === "QB1")
+                            .map((rp) => {
+                                return (
+                                    <RosterPlayerCard
+                                        rp={rp}
+                                        key={`rp-${rp.rosterPlayerId}`}
+                                    ></RosterPlayerCard>
+                                )
+                        }) : 
+                        <BlankRosterPlayerCard position={"QB1"}/>
+                    }
+                    {roster.rosterPlayers.some((rp) => rp.rosterPosition === "WR1") ?
+                        roster.rosterPlayers
+                            .filter((rp) => rp.rosterPosition === "WR1")
+                            .map((rp) => {
+                                return (
+                                    <RosterPlayerCard
+                                        rp={rp}
+                                        key={`rp-${rp.rosterPlayerId}`}
+                                    ></RosterPlayerCard>
+                                )
+                        }) : 
+                        <BlankRosterPlayerCard position={"WR1"}/>
+                    }
+                    {roster.rosterPlayers.some((rp) => rp.rosterPosition === "WR2") ?
+                        roster.rosterPlayers
+                            .filter((rp) => rp.rosterPosition === "WR2")
+                            .map((rp) => {
+                                return (
+                                    <RosterPlayerCard
+                                        rp={rp}
+                                        key={`rp-${rp.rosterPlayerId}`}
+                                    ></RosterPlayerCard>
+                                )
+                        }) : 
+                        <BlankRosterPlayerCard position={"WR2"}/>
+                    }
+                    {roster.rosterPlayers.some((rp) => rp.rosterPosition === "RB1") ?
+                        roster.rosterPlayers
+                            .filter((rp) => rp.rosterPosition === "RB1")
+                            .map((rp) => {
+                                return (
+                                    <RosterPlayerCard
+                                        rp={rp}
+                                        key={`rp-${rp.rosterPlayerId}`}
+                                    ></RosterPlayerCard>
+                                )
+                        }) : 
+                        <BlankRosterPlayerCard position={"RB1"}/>
+                    }
+                    {roster.rosterPlayers.some((rp) => rp.rosterPosition === "RB2") ?
+                        roster.rosterPlayers
+                            .filter((rp) => rp.rosterPosition === "RB2")
+                            .map((rp) => {
+                                return (
+                                    <RosterPlayerCard
+                                        rp={rp}
+                                        key={`rp-${rp.rosterPlayerId}`}
+                                    ></RosterPlayerCard>
+                                )
+                        }) : 
+                        <BlankRosterPlayerCard position={"RB2"}/>
+                    }
+                    {roster.rosterPlayers.some((rp) => rp.rosterPosition === "TE1") ?
+                        roster.rosterPlayers
+                            .filter((rp) => rp.rosterPosition === "TE1")
+                            .map((rp) => {
+                                return (
+                                    <RosterPlayerCard
+                                        rp={rp}
+                                        key={`rp-${rp.rosterPlayerId}`}
+                                    ></RosterPlayerCard>
+                                )
+                        }) : 
+                        <BlankRosterPlayerCard position={"TE1"}/>
+                    }
+                    {roster.rosterPlayers.some((rp) => rp.rosterPosition === "FLEX") ?
+                        roster.rosterPlayers
+                            .filter((rp) => rp.rosterPosition === "FLEX")
+                            .map((rp) => {
+                                return (
+                                    <RosterPlayerCard
+                                        rp={rp}
+                                        key={`rp-${rp.rosterPlayerId}`}
+                                    ></RosterPlayerCard>
+                                )
+                        }) : 
+                        <BlankRosterPlayerCard position={"FLEX"}/>
+                    }
+                    {roster.rosterPlayers.some((rp) => rp.rosterPosition === "K") ?
+                        roster.rosterPlayers
+                            .filter((rp) => rp.rosterPosition === "K")
+                            .map((rp) => {
+                                return (
+                                    <RosterPlayerCard
+                                        rp={rp}
+                                        key={`rp-${rp.rosterPlayerId}`}
+                                    ></RosterPlayerCard>
+                                )
+                        }) : 
+                        <BlankRosterPlayerCard position={"K"}/>
+                    }
+                    {roster.rosterPlayers.some((rp) => rp.rosterPosition === "DEF") ?
+                        roster.rosterPlayers
+                            .filter((rp) => rp.rosterPosition === "DEF")
+                            .map((rp) => {
+                                return (
+                                    <RosterPlayerCard
+                                        rp={rp}
+                                        key={`rp-${rp.rosterPlayerId}`}
+                                    ></RosterPlayerCard>
+                                )
+                        }) : 
+                        <BlankRosterPlayerCard position={"DEF"}/>
+                    }
                 </tbody>
                 <tbody className="table-divider">
                     {roster.rosterPlayers
                         .filter((rp) => rp.rosterPosition === "bench")
-                        .map((rp) => { // change to map only RP that are benched
+                        .map((rp) => {
                         return (
                             <RosterPlayerCard
                                 rp={rp}
