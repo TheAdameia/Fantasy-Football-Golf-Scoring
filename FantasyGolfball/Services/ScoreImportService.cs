93182a5f5d9dd98526553e83e7ca7009ec9fce6c
Adameia Ramsey <adameiaproductions@gmail.com>
2025-07-24 13:45:53 -0500

    saving

--- FantasyGolfball/Services/ScoreImportService.cs
+++ FantasyGolfball/Services/ScoreImportService.cs
@@ -1,4 +1,8 @@
-
+using System.Globalization;
+using CsvHelper;
+using FantasyGolfball.Data;
+using FantasyGolfball.Models.Test;
+using Microsoft.EntityFrameworkCore;
 
 
 // this will assuredly be a horrible kluge of switch case due to how the data is formatted.
@@ -7,3 +11,84 @@
 
 // It would probably be better to make the scraper a kluge so that if I ever have to use a different
 // source, I can re-use the service by having the scraper match the format.
+
+namespace FantasyGolfball.Services;
+// make sure to instantiate this in program.cs
+
+public class ScoringCvsRow
+{
+    public int Completions { get; set; }
+    public int AttemptsPassing { get; set; }
+    public int YardsPassing { get; set; }
+    public int TouchdownsPassing { get; set; }
+    public int Interceptions { get; set; }
+    public int Targets { get; set; }
+    public int Receptions { get; set; }
+    public int YardsReceiving { get; set; }
+    public int TouchdownsReceiving { get; set; }
+    public int AttemptsRushing { get; set; }
+    public int YardsRushing { get; set; }
+    public int TouchdownsRushing { get; set; }
+    public int Fumbles { get; set; }
+    public int FumblesLost { get; set; }
+    public int TwoExtraPoints { get; set; }
+    public int FieldGoalAttempts { get; set; }
+    public int FieldGoalsMade { get; set; }
+    public int ExtraPointAttempts { get; set; }
+    public int ExtraPointMade { get; set; }
+    public int PointsAgainst { get; set; }
+    public int Sacks { get; set; }
+    public int InterceptionDefense { get; set; }
+    public int DefenseFumbleRecovery { get; set; }
+    public int Safety { get; set; }
+    public int TouchdownsDefense { get; set; }
+    public int TouchdownsReturn { get; set; }
+    public int BlockedKicks { get; set; }
+    public int FantasyPoints { get; set; }
+    public string Week { get; set; }
+    public string PlayerID { get; set; }
+    public string Position { get; set; }
+}
+
+public interface IScoringImportService
+{
+    Task<int> ImportScoringFromCsvAsync(Stream fileStream, int seasonId, CancellationToken cancellationToken);
+}
+
+public class ScoringImportService : IScoringImportService
+{
+    private readonly IServiceScopeFactory _scopeFactory;
+
+    public ScoringImportService(IServiceScopeFactory scopeFactory)
+    {
+        _scopeFactory = scopeFactory;
+    }
+
+    public async Task<int> ImportScoringFromCsvAsync(Stream fileStream, int seasonId, CancellationToken cancellationToken)
+    {
+        using var scope = _scopeFactory.CreateScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<FantasyGolfballDbContext>();
+
+        var scoresToAdd = new List<NewScoringTest>();
+
+        using var reader = new StreamReader(fileStream);
+        using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+        var records = csv.GetRecords<PlayerCsvRow>();
+
+        foreach (var row in records)
+        {
+            // iterate through each row
+            // find the right player to assign the PlayerId
+            // create the scoring
+
+            var newScoring = new NewScoringTest
+            {
+                SeasonId = seasonId,
+                IsDefense = false,
+                PlayerId = ,
+                
+            };
+            
+        }
+    }
+}
