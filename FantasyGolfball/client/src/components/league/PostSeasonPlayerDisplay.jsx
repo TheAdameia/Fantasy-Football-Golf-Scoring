20f8c53f92c3d9e7c90a3c7cd9ed9d75041ca70e
Adameia Ramsey <adameiaproductions@gmail.com>
2026-02-12 17:02:44 -0600

    saving

--- FantasyGolfball/client/src/components/league/PostSeasonPlayerDisplay.jsx
+++ FantasyGolfball/client/src/components/league/PostSeasonPlayerDisplay.jsx
@@ -3,9 +3,26 @@ import { useAppContext } from "../../contexts/AppContext"
 
 
 export const PostSeasonPlayerDisplay = ({user, index, playerId}) => {
-    const { selectedLeague, players } = useAppContext()
+    const { selectedLeague, players, allScores } = useAppContext()
     const [player, setPlayer] = useState()
+    const [filteredScores, setFilteredSCores] = useState()
+    const [totalWithPenalties, setTotalWithPenalties] = useState()
     const drafter = selectedLeague.leagueUsers.find(lu => lu.userProfileId == user)
+
+    const GetTotalWithPenalties = () => {
+        let totalPoints = 0
+        for (const score of filteredScores) {
+            // insert penalty check here
+            totalPoints += score.points
+        }
+
+        // accounts for weeks with no Scoring
+        if (filteredScores.length() < selectedLeague.season.seasonWeeks) {
+            const additionalPenalty = (selectedLeague.season.seasonWeeks - filteredScores.length()) * 15
+            totalPoints += additionalPenalty
+        }
+        setTotalWithPenalties(totalPoints)
+    }
 
 
     useEffect(() => {
@@ -14,6 +31,13 @@ export const PostSeasonPlayerDisplay = ({user, index, playerId}) => {
             setPlayer(foundPlayer)
         }
     }, [playerId, players])
+
+    useEffect(() => {
+        if (allScores) {
+            const filterScore = allScores.filter(s => s.playerId == playerId)
+            setFilteredSCores(filterScore)
+        }
+    }, [allScores, playerId])
 
     if (player) {
         return (
